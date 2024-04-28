3d4c128b27d255fc35cdbadbd7bf88c4086f4c0a
Sebastian298 <jaimetenoriorios@outlook.com>
2024-04-28 14:21:44 -0500

    creation of app_theme class for configuration of theme color in the app

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.white,
+  Colors.purple,
+  Colors.orange,
+  Colors.indigo,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey,
+  Colors.black,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({required this.selectedColor})
+      : assert(
+          selectedColor >= 0,
+          'selectedColor must be greater than 0',
+        ),
+        assert(
+          selectedColor < colorList.length,
+          'selectedColor must be less or equal than ${colorList.length - 1}',
+        );
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+        ),
+      );
+}
